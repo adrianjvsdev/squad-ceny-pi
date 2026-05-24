7e1e969573fe27d43c51701e3a5bb5ecfdcfbf87
Purplefloydds <Purplefloydds@gmail.com>
2026-05-23 21:05:18 -0300

    feat: faz inventário funcionar

--- backend/manutencao/serializers.py
+++ backend/manutencao/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import PlanoManutencao
+from .models import AnomaliaIoT, PlanoManutencao
 
 
 class PlanoManutencaoSerializer(serializers.ModelSerializer):
@@ -27,3 +27,36 @@ class PlanoManutencaoSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError("A periodicidade deve ser maior que zero.")
         return value
+
+
+class AnomaliaIoTSerializer(serializers.ModelSerializer):
+    equipamento_tag = serializers.CharField(source="equipamento.tag", read_only=True)
+    equipamento_nome = serializers.CharField(source="equipamento.nome", read_only=True)
+
+    class Meta:
+        model = AnomaliaIoT
+        fields = [
+            "id",
+            "equipamento",
+            "equipamento_tag",
+            "equipamento_nome",
+            "tipo",
+            "valor",
+            "valor_limite",
+            "severidade",
+            "detectada_em",
+        ]
+        read_only_fields = ["id", "detectada_em"]
+
+
+class IoTStatusSerializer(serializers.Serializer):
+    id_equipamento = serializers.IntegerField()
+    tag = serializers.CharField()
+    nome = serializers.CharField()
+    tem_iot = serializers.BooleanField()
+    temperatura = serializers.FloatField(allow_null=True)
+    rpm = serializers.FloatField(allow_null=True)
+    pressao = serializers.FloatField(allow_null=True)
+    anomalias_recentes = AnomaliaIoTSerializer(many=True, read_only=True)
+    status_geral = serializers.CharField()  # "normal", "alerta", "critico"
+    
